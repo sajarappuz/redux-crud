6c9665d7626fce7b3d40cc4257567a8cd748f972
Sajarappuz <sajarmohammed71@gmail.com>
2023-03-29 12:31:36 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useSelector } from 'react-redux'; //to read value
+import { addUser , deleteUser, updateUser} from './features/Users';
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
 
 function App() {
+  const [name,setName] = useState("");
+  const [username,setUsername] = useState("");
+  const [newusername,setNewUsername] = useState("");
+  const dispatch = useDispatch();
+  const userList = useSelector((state) => state.users.value);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="adduser">
+        <input type="text" placeholder='name' onChange={(event)=> {setName(event.target.value);}} />
+        <input type="text" placeholder='username' onChange={(event)=> {setUsername(event.target.value);}}/>
+        <button onClick={() =>{dispatch(addUser({id: userList[userList.length - 1].id + 1, name , username }))}}> Add User</button>
+      </div>
+      <div className="displayuser">
+        {userList.map((user) => {
+          return(
+            <div>
+            <h1>{user.name}</h1>
+            <h1>{user.username}</h1>
+            <input type="text" placeholder='newusername' onChange={(event) => {setNewUsername(event.target.value);}}/>
+            <button onClick={() =>  {dispatch(updateUser({id:user.id, username:newusername}))}}>Update Username</button>
+            <button onClick={()=> {dispatch(deleteUser({id:user.dispatch}))}}>Delete user</button>
+            </div>
+          )
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/Data.js
@@ -0,0 +1,42 @@
+export const UsersData = [
+    {
+      id: 1,
+      name: "Sam Billei",
+      username: "Sambillie",
+    },
+    {
+      id: 2,
+      name: "Loius Samuel",
+      username: "Lui",
+    },
+    {
+      id: 3,
+      name: "Rahul Raju",
+      username: "rahulr",
+    },
+    {
+      id: 4,
+      name: "Patricia rodrigues",
+      username: "Karianne",
+    },
+    {
+      id: 5,
+      name: "Jhon doe",
+      username: "Kamren",
+    },
+    {
+      id: 6,
+      name: "Jake Runolfsdottir V",
+      username: "Leopoldo_Corkery",
+    },
+    {
+      id: 7,
+      name: "Mrs. Dennis Schulist",
+      username: "Karley_Dach",
+    },
+    {
+      id: 8,
+      name: "Kurtis Weissnat",
+      username: "Elwyn.Skiles",
+    },
+  ];
